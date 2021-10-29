52444ed67d6ef13a23120173a4be595ce2a8c1bb
Pedro Balestra <pedro.balestra@gec.inate.br>
2021-10-29 16:00:06 -0300

    LIdando com os estados do snapshot

--- lib/srceens/contacts_list.dart
+++ lib/srceens/contacts_list.dart
@@ -14,22 +14,53 @@ class ContactsList extends StatelessWidget {
       ),
       // inserido generics no FutureBuilder
       body: FutureBuilder<List<Contact>>(
+        initialData: const [],
         future: findAll(),
         builder: (context, snapshot) {
-          final List<Contact>? contacts = snapshot
-              .data; // usado null safe na lista pois é regra da nova versão do flutter
-          if (contacts != null) {
-            //feito if para validar nulo
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Contact contact = contacts[index];
-                return _ContactItem(contact);
-              },
-              itemCount: contacts.length,
-            );
-          }
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
 
-          return Container();
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+
+            case ConnectionState.active:
+              break;
+
+            case ConnectionState.done:
+              final List<Contact>? contacts = snapshot
+                  .data; // usado null safe na lista pois é regra da nova versão do flutter
+              if (contacts != null) {
+                //feito if para validar nulo
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Contact contact = contacts[index];
+                    return _ContactItem(contact);
+                  },
+                  itemCount: contacts.length,
+                );
+              }
+              break;
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('Unknown error',
+                    style: TextStyle(color: Colors.red, fontSize: 24.0)),
+              ],
+            ),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
